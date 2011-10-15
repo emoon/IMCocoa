80e07d2494b5cab4bd2ef9fb65275b623344900f
Daniel Collin <daniel@collin.com>
2011-10-15 21:43:16 +0200

    Added wrapper for Common Lisp

---
+++ src/wrappers/common-lisp/imcocoa.lisp
@@ -0,0 +1,35 @@
+;;; TODO: Fix Some ugly hard-coded paths here for testing
+
+(cffi:define-foreign-library imcocoa-lib 
+  (:darwin (:or "~/code/IMCocoa/tundra-output/macosx-gcc-debug-defalut/libIMCocoa.dylib"
+                "libIMCocoa.dylib")))
+
+(cffi:use-foreign-library imcocoa-lib)
+
+;; App
+
+(cffi:defcfun ("IMCocoa_appCreate" imcocoa-app-create) :void ())
+(cffi:defcfun ("IMCocoa_appRun" imcocoa-app-run) :void ())
+(cffi:defcfun ("IMCocoa_appDestroy" imcocoa-app-destroy) :void ())
+
+;; Windows
+
+(cffi:defcfun ("IMCocoa_windowCreate" imcocoa-window-create) :pointer
+	(name :string)
+	(callback :pointer)
+	(user-data :pointer))
+
+(cffi:defcfun ("IMCocoa_windowDestroy" imcocoa-window-destroy) :void
+	(handle :pointer))
+
+;;; Controls
+
+(cffi:defcfun ("IMCocoa_buttonCall" imcocoa-button) :int
+	(id :int)
+	(parent :pointer) 
+	(name :string) 
+	(x :int) 
+	(y :int) 
+	(w :int) 
+	(h :int))
+
